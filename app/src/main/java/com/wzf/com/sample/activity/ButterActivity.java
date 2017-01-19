d9ce1b5536f5d20ad61d83ee2a1647b506c10b87
soonlen <soonlen@163.com>
2017-01-19 17:29:35 +0800

    添加了Butter注入式框架案例

---
+++ app/src/main/java/com/wzf/com/sample/activity/ButterActivity.java
@@ -0,0 +1,59 @@
+package com.wzf.com.sample.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckedTextView;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.wzf.com.sample.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by soonlen on 2017/1/19.
+ * 框架说明地址：http://jakewharton.github.io/butterknife/
+ * 1、强大的View绑定和Click事件处理功能，简化代码，提升开发效率
+ * 2、方便的处理Adapter里的ViewHolder绑定问题
+ * 3、运行时不会影响APP效率，使用配置方便
+ * 4、代码清晰，可读性强
+ * 使用：
+ * dependencies{
+ * compile 'com.jakewharton:butterknife:8.4.0'
+ * annotationProcessor 'com.jakewharton:butterknife-compiler:8.4.0'
+ * }
+ */
+
+public class ButterActivity extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView tvName;
+    @BindView(R.id.button4)
+    Button btnTest;
+    @BindView(R.id.toggleButton)
+    ToggleButton tbTest;
+    @BindView(R.id.radioButton)
+    RadioButton radioButton;
+    @BindView(R.id.checkedTextView)
+    CheckedTextView checkedTextView;
+    @BindView(R.id.seekBar)
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_butter);
+        ButterKnife.bind(this);
+        tvName.setText("这是测试Butter注入框架使用");
+        btnTest.setText("按钮……");
+        tbTest.setChecked(true);
+        radioButton.toggle();
+        checkedTextView.toggle();
+        seekBar.setProgress(50);
+    }
+
+}
